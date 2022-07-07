de7a923f8f109b634e563c3927b8801dd1b11434
frvnkhvrt <frvnkhvrt@gmail.com>
2022-07-07 17:39:52 -0500

    added splash screen

--- assets/js/splash.js
+++ assets/js/splash.js
@@ -1,7 +1,7 @@
 /*==================== SPLASH SCREEN ====================*/
 let intro = document.querySelector('.intro');
-let logo = document.querySelector('.logo-header');
-let logoSpan = document.querySelector('.logo');
+let logoHeader = document.querySelector('.logo__header');
+let logoSpan = document.querySelectorAll('.logoSplash');
 
 window.addEventListener('DOMContentLoaded', () => {
     
@@ -9,9 +9,21 @@ window.addEventListener('DOMContentLoaded', () => {
 
         logoSpan.forEach((span, idx)=>{
             setTimeout(() => {
-                span.classList.add('active')
+                span.classList.add('active');
             }, (idx + 1) * 400)
-        })
+        });
+        
+        setTimeout(() => {
+            logoSpan.forEach((span, idx)=>{
+                setTimeout(() => {
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx + 1) * 50)
+            })    
+        },2000);
 
+        setTimeout(() => {
+            intro.style.top = '-100vh';
+        },2300);
     })
 })
